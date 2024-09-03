ee8be4fa48cd8a5668d4df4901989e6a7b328243
ManuelRoperoM <mnlrpr@gmail.com>
2024-09-02 19:21:17 -0500

    Creacion nuevas vistas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
 import { io } from 'socket.io-client'
-
+import Icon from '@mdi/react'
+import { mdiContentSaveAll, mdiHistory, mdiSineWave } from '@mdi/js'
+import PowerTriangleChart from './components/chart'
 const socket = io('http://192.168.1.15:8081')
 function App () {
-  const [data, setData] = useState(null)
+  // const [data, setData] = useState(null)
+  const [data, setData] = useState({
+    potenciaP: 150,
+    reactivaQ: 20,
+    aparenteS: 107.703,
+    factorPotencia: 1
+  })
+
   const [timeoutId, setTimeoutId] = useState(null)
   useEffect(() => {
     socket.on('data', (newData) => {
@@ -31,12 +40,20 @@ function App () {
     if (data) {
       try {
         return (
-          <div>
+          <div className='metering'>
             <h3>Medicion en curso</h3>
-            <p>Potencia P: {data.potenciaP}</p>
-            <p>Reactiva Q: {data.reactivaQ}</p>
-            <p>Aparente S: {data.aparenteS}</p>
-            <p>Factor de potencia: {data.factorPotencia}</p>
+            <div className='data'>
+              <div className='info'>
+                <p>Potencia P: {data.potenciaP} W</p>
+                <p>Reactiva Q: {data.reactivaQ} VA</p>
+                <p>Aparente S: {data.aparenteS} VAR</p>
+                <p>Factor de potencia: {data.factorPotencia}</p>
+              </div>
+              <div className='grafica'>
+                <PowerTriangleChart data={data} />
+              </div>
+            </div>
+            <button><Icon path={mdiContentSaveAll} size={1} /></button>
           </div>
         )
       } catch (e) {
@@ -48,7 +65,13 @@ function App () {
   }
   return (
     <>
-      <h1>Lifae Medicion de potencia IOT - lifae</h1>
+      <div className='header'>
+        <h1>Lifae Medicion de potencia IOT - lifae</h1>
+        <div className='header__buttons'>
+          <button><Icon path={mdiHistory} size={1} /> Historial</button>
+          <button><Icon path={mdiSineWave} size={1} />Medición</button>
+        </div>
+      </div>
       <p>{renderData()}</p>
     </>
   )
